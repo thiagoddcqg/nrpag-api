ef13383ca7b47646ad8f844e5c90adc462ddcef2
Thiago Gama <thiagoddcqg@gmail.com>
2024-02-17 23:29:35 -0300

    Vai

---
+++ src/main/java/com/nextreleaseproblem/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.nextreleaseproblem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("src.main.java.com.nextreleaseproblem.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API REST com Spring Boot")
+                .description("Documentação da API REST gerada pelo Swagger")
+                .version("1.0.0")
+                .build();
+    }
+}
