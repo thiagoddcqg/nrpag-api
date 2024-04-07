573b9195a8f158b862d61f9097407dd378d2db9d
Thiago Gama <thiagoddcqg@gmail.com>
2024-04-06 23:06:14 -0300

    Vai

--- src/main/java/com/nextreleaseproblem/NrpagApiApplication.java
+++ src/main/java/com/nextreleaseproblem/NrpagApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetaheuristicasApplication {
+public class NrpagApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetaheuristicasApplication.class, args);
+        SpringApplication.run(NrpagApiApplication.class, args);
     }
 }
