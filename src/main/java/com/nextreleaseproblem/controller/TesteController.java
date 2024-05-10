43586d9862cd3b19d36a7971158e1d74a653de98
RicardoMGM <ricardo.manel@gmail.com>
2024-05-10 15:52:34 -0300

    vai

---
+++ src/main/java/com/nextreleaseproblem/controller/TesteController.java
@@ -0,0 +1,20 @@
+package com.nextreleaseproblem.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/teste")
+public class TesteController {
+
+    @GetMapping("")
+    public String teste(Model model) {
+        model.addAttribute("message", "Sucesso!, conexão estabalecida.");
+        return "teste";
+    }
+
+}
